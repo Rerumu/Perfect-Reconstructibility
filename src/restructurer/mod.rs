831c3817dc58fcb9d0430faf0037d5bb62ce30cc
Rerumu <25379555+Rerumu@users.noreply.github.com>
2024-01-23 07:27:36 -0500

    Replacement

--- src/restructurer/mod.rs
+++ src/restructurer/mod.rs
@@ -3,6 +3,5 @@ mod depth_first_searcher;
 mod dominator_finder;
 mod repeat;
 mod strongly_connected_finder;
-mod strongly_connected_finder2;
 
 pub mod linear;
--- src/restructurer/strongly_connected_finder.rs
+++ src/restructurer/strongly_connected_finder.rs
@@ -1,139 +1,105 @@
+// https://www.cs.princeton.edu/courses/archive/spr04/cos423/handouts/path%20based...pdf
+
 use crate::control_flow::Nodes;
 
-enum Record {
-	Unseen,
-	Seen,
-	Named { lowest: u32 },
-}
+use super::depth_first_searcher::DepthFirstSearcher;
 
-impl Record {
-	const fn lowest(&self) -> u32 {
-		match self {
-			Self::Unseen | Self::Seen => u32::MAX,
-			Self::Named { lowest } => *lowest,
-		}
-	}
-
-	fn set_lowest(&mut self, value: u32) {
-		match self {
-			Self::Unseen | Self::Seen => {}
-			Self::Named { lowest } => *lowest = value.min(*lowest),
-		}
-	}
-}
-
-struct Item {
-	name: u32,
-	id: usize,
-	successors: Vec<usize>,
-}
-
+#[derive(Default)]
 pub struct StronglyConnectedFinder {
+	names: Vec<usize>,
+	path: Vec<usize>,
+	stack: Vec<usize>,
+
 	results: Vec<Vec<usize>>,
-	records: Vec<Record>,
-	history: Vec<usize>,
-	items: Vec<Item>,
-	names: u32,
+	vec_unused: Vec<Vec<usize>>,
+	depth_first_searcher: DepthFirstSearcher,
 }
 
 impl StronglyConnectedFinder {
-	#[must_use]
 	pub const fn new() -> Self {
 		Self {
+			names: Vec::new(),
+			path: Vec::new(),
+			stack: Vec::new(),
+
 			results: Vec::new(),
-			records: Vec::new(),
-			history: Vec::new(),
-			items: Vec::new(),
-			names: 0,
+			vec_unused: Vec::new(),
+			depth_first_searcher: DepthFirstSearcher::new(),
 		}
 	}
 
-	fn initialize_fields(&mut self, record_count: usize) {
-		self.results.clear();
-		self.records.clear();
-		self.records.resize_with(record_count, || Record::Seen);
+	fn initialize_fields<N: Nodes>(&mut self, nodes: &N) {
+		let last_id = nodes.iter().max().map_or(0, |id| id + 1);
 
-		self.names = 0;
+		self.names.clear();
+		self.names.resize(last_id, usize::MAX);
+
+		self.vec_unused.append(&mut self.results);
 	}
 
-	fn initialize_item<N: Nodes>(&mut self, nodes: &N, id: usize) {
-		let successors = nodes.successors(id).collect();
+	fn on_pre_order<N: Nodes>(&mut self, nodes: &N, id: usize) {
+		let index = self.path.len();
 
-		self.history.push(id);
-		self.items.push(Item {
-			name: self.names,
-			id,
-			successors,
-		});
+		self.names[id] = index;
 
-		self.records[id] = Record::Named { lowest: self.names };
-		self.names += 1;
-	}
+		self.path.push(id);
+		self.stack.push(index);
 
-	fn finalize_item(&mut self, id: usize) {
-		let position = self.history.iter().rposition(|&other| id == other).unwrap();
+		for successor in nodes.successors(id).filter(|&id| id != usize::MAX) {
+			if let Some(&index) = self.names.get(successor) {
+				let last = self.stack.iter().rposition(|&id| id <= index).unwrap();
 
-		for &id in &self.history[position..] {
-			self.records[id] = Record::Seen;
-		}
-
-		let result = self.history.drain(position..);
-
-		if result.len() > 1 {
-			self.results.push(result.collect());
-		}
-	}
-
-	#[allow(clippy::match_on_vec_items)]
-	fn handle_from_successor<N: Nodes>(&mut self, nodes: &N, id: usize, successor: usize) {
-		match *self.records.get(successor).unwrap_or(&Record::Seen) {
-			Record::Unseen => self.initialize_item(nodes, successor),
-			Record::Seen => {}
-			Record::Named { lowest } => self.records[id].set_lowest(lowest),
-		}
-	}
-
-	fn handle_to_predecessor(&mut self, predecessor: usize, id: usize) {
-		let lowest = self.records[id].lowest();
-
-		self.records[predecessor].set_lowest(lowest);
-	}
-
-	fn run_at_position<N: Nodes>(&mut self, nodes: &N, start: usize) {
-		self.handle_from_successor(nodes, start, start);
-
-		while let Some(mut item @ Item { name, id, .. }) = self.items.pop() {
-			if let Some(successor) = item.successors.pop() {
-				self.items.push(item);
-
-				self.handle_from_successor(nodes, id, successor);
-			} else {
-				if let Some(predecessor) = self.items.last() {
-					self.handle_to_predecessor(predecessor.id, id);
-				}
-
-				if name == self.records[id].lowest() {
-					self.finalize_item(id);
-				}
+				self.stack.truncate(last + 1);
 			}
 		}
 	}
 
+	fn on_post_order(&mut self, id: usize) {
+		let index = self.stack.pop().unwrap();
+
+		if self.names[id] != index {
+			self.stack.push(index);
+
+			return;
+		}
+
+		let mut result = self.vec_unused.pop().unwrap_or_default();
+
+		result.clear();
+		result.extend(self.path.drain(index..));
+
+		for &id in &result {
+			self.names[id] = usize::MAX;
+		}
+
+		if result.len() > 1 {
+			self.results.push(result);
+		} else {
+			self.vec_unused.push(result);
+		}
+	}
+
 	pub fn run<N: Nodes>(&mut self, nodes: &N) -> &mut Vec<Vec<usize>> {
-		let record_count = nodes.iter().max().map_or(0, |id| id + 1);
+		let mut depth_first_searcher = std::mem::take(&mut self.depth_first_searcher);
 
-		self.initialize_fields(record_count);
+		depth_first_searcher.initialize(nodes);
 
-		for id in nodes.iter() {
-			self.records[id] = Record::Unseen;
-		}
+		self.initialize_fields(nodes);
 
 		for id in nodes.iter() {
-			self.run_at_position(nodes, id);
+			depth_first_searcher.run(nodes, id, |id, post| {
+				if post {
+					self.on_post_order(id);
+				} else {
+					self.on_pre_order(nodes, id);
+				}
+			});
 		}
 
-		debug_assert!(self.items.is_empty(), "items is not empty");
-		debug_assert!(self.history.is_empty(), "history is not empty");
+		self.depth_first_searcher = depth_first_searcher;
+
+		debug_assert!(self.path.is_empty(), "path is not empty");
+		debug_assert!(self.stack.is_empty(), "stack is not empty");
 
 		&mut self.results
 	}
